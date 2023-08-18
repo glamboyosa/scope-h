a43ce8dcc2384846a5716febdaea1719bad5f0e0
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-08-18 22:50:18 +0100

    feat: add shadcn

--- opt/client/vite.config.ts
+++ opt/client/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,6 +10,11 @@ export default defineConfig({
     strictPort: true,
     hmr: {
       port: 3000,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
